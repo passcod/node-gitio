ba755a2ae29b1bc80db28b4b155e1f7f6104f973
Félix Saparelli <felix@passcod.name>
2015-02-22 22:42:44 +1300

    Cut new release

--- test.js
+++ test.js
@@ -1,3 +1,25 @@
 var gitio = require('./index');
 
-gitio('https://github.com/tanepiper/node-gitio', console.log);
+var urls = [
+  ["https://github.com/passcod/node-gitio", "https://git.io/AuAj"],
+  ["https://passcod.name", "[Error: Git.io 500 Internal Server Error]"]
+];
+
+console.log("Input, Expected output");
+urls.forEach(function(url) {
+  var source = url[0];
+  var result = url[1];
+  console.log(source + ', ' + result);
+});
+console.log("-----");
+console.log("(Results may not be in order)");
+
+urls.forEach(function(source) {
+  gitio(source[0], function(err, url) {
+    if (err) {
+      console.log("Got error: ", err);
+    } else {
+      console.log("Got URL: ", url);
+    }
+  });
+});
